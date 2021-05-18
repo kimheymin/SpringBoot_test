7819ead067a4f5d5267f9d1ef094449ea0ae0277
hm <keg9317@naver.com>
2021-05-18 23:30:34 +0900

    테스트코드 작성 후 테스트 중...

---
+++ src/main/java/com/test/springboot/Application.java
@@ -0,0 +1,12 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication //스프링 부트 자동 설정, 스프링빈 읽기와 생성 모두 자동으로 설정
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+        //SpringBootApplication.run 으로 인해 내장 WAS 실행
+    }
+}
---
+++ src/test/java/com/test/springboot/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.test.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class) //스프링부트 테스트와 JUnit 사이 연결자 역할
+@WebMvcTest(controllers = HelloController.class) //web에 집중할 수 있는 어노테이션
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc; //테스트의 시작점
+
+    @Test
+    public void hello가_리턴된다() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello")) //MockMvc통해 /hello 주소로 HTTP GET 요청 (체이닝 지원)
+                .andExpect(status().isOk()) //200인지 아닌지 검증
+                .andExpect(content().string(hello)); //컨트롤러에서 'hello' 리턴하기 때문에 이 값이 맞는지 검증
+
+    }
+
+}
