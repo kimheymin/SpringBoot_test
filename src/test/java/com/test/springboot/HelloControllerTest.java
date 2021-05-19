0b35303097d51f1203ed38fa7c5b6432cd7aa1d1
hm <keg9317@naver.com>
2021-05-19 14:14:12 +0900

    롬복 테스트 코드 적용

--- src/test/java/com/test/springboot/HelloControllerTest.java
+++ src/test/java/com/test/springboot/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //스프링부트 테스트와 JUnit 사이 연결자 역할
 @WebMvcTest(controllers = HelloController.class) //web에 집중할 수 있는 어노테이션
@@ -26,7 +26,23 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) //MockMvc통해 /hello 주소로 HTTP GET 요청 (체이닝 지원)
                 .andExpect(status().isOk()) //200인지 아닌지 검증
                 .andExpect(content().string(hello)); //컨트롤러에서 'hello' 리턴하기 때문에 이 값이 맞는지 검증
-
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name",name)
+                        .param("amount",String.valueOf(amount)) //param에는 문자열만 가능해서 문자열로 변경
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name)))
+                .andExpect(jsonPath("$.amount",is(amount)));
+                //jsonPath : 응답값을 필드별로 검증할 수 있는 메소드 / $을 기준으로 필드명 명시
+    }
+
 
 }
