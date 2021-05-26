e526c9df33940978f831a8cad195c58eab612c31
hm <keg9317@naver.com>
2021-05-26 12:37:38 +0900

    게시글 수정 화면 및 기능 추가

--- src/main/java/com/test/springboot/web/IndexController.java
+++ src/main/java/com/test/springboot/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.test.springboot.web;
 
 import com.test.springboot.service.posts.PostService;
+import com.test.springboot.web.dto.PostResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,5 +28,12 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("posts",postService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/upste/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostResponseDto dto = postService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
     }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,10 +1,16 @@
 var main = {
     init : function(){
         var_this = this;
+
         $('#btn-save').on('click',function(){
             _this.save();
         });
+
+         $('#btn-update').on('click',function(){
+            _this.update();
+         });
     },
+
     save : function(){
         var data = {
             title : $('#title').val();
@@ -24,5 +30,29 @@ var main = {
         }),fail(function (error){
            alert(JSON.stringify(error));
         });
-    }
+    },
+
+    update : function(){
+        var data = {
+            title : $('#title').val();
+            content : $('#content').val();
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+                type: 'PUT',
+                url : '/api/v1/posts'+id,
+                dateType : 'json',
+                contentType : 'application/json; charset=utf-8',
+                data : JSON.stringify(data)
+            }).done(function(){
+                alert('글 수정 완료!');
+                window.application.href='/'
+            }),fail(function (error){
+               alert(JSON.stringify(error));
+            });
+        }
 };
+
+main.init();
