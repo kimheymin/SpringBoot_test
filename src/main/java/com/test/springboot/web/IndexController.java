c3cf7848ffe4f73d930a2dd5a7f8324a6e51b830
hm <keg9317@naver.com>
2021-06-02 17:23:59 +0900

    세션 저장소 db로 변경

--- src/main/java/com/test/springboot/web/IndexController.java
+++ src/main/java/com/test/springboot/web/IndexController.java
@@ -14,22 +14,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-/*    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
-
-    @GetMapping("/posts/save")
-    public String postSave(){
-        return "posts-save";
-    }*/
-
     private final PostService postService;
 
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postService.findAllDesc());
-        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user!=null){
             model.addAttribute("userName",user.getName());
         }
